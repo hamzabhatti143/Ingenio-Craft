d2c1ea16bd953a7d478bc2f23acb4c599503f136
Hamza Bhatti <104676544+hamzabhatti143@users.noreply.github.com>
2025-02-17 16:07:52 +0500

    Content Update

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -12,9 +12,7 @@ const About = () => {
       <div className='w-full flex flex-col md:flex-row bg-gray-800'>
         <div data-aos="fade-right" data-aos-duration="1200" className='w-full md:w-1/2 p-6'>
         <h1 className='font-bold text-3xl text-center text-white'>About Us</h1>
-        <p className='text-justify text-white'>At IngenioCraft, we specialize in providing high-quality engineering solutions using cutting-edge tools like Ansys, AutoCAD, and CFD simulations. Our expert team is dedicated to delivering precise structural analysis, fluid flow simulations, and advanced CAD designs to help businesses optimize their projects with accuracy and efficiency.
-
-With a strong commitment to innovation and excellence, we empower industries ranging from automotive, aerospace, and construction to energy and manufacturing. Whether its Finite Element Analysis (FEA), Computational Fluid Dynamics (CFD), or detailed CAD drafting, we ensure that our clients receive the best solutions tailored to their specific needs.</p>
+        <p className='text-justify text-white'>At IngenioCraft, we deliver top-tier engineering solutions using Ansys, AutoCAD, and CFD simulations. Our experts provide precise structural analysis, fluid flow simulations, and CAD designs to optimize projects with accuracy and efficiency. Serving industries like automotive, aerospace, and manufacturing, we specialize in FEA, CFD, and CAD drafting, ensuring tailored, high-quality results.</p>
         </div>
         <div data-aos="fade-left" data-aos-duration="1200" className='w-full md:w-1/2 flex items-center justify-center'>
             <Image src={"/images/about.jpg"} alt='about' width={100} height={100} className='w-3/4 rounded-lg '/>
